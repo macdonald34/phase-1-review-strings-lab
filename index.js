1ef16c129d3180dbf8ebf54fd2c252d6d9920a52
macdonald34 <macdonald.muhavi@student.moringaschool.com>
2023-12-13 09:43:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const currentUser = 'Macdonald Muhavi';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK.toUpperCase();!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK,MACDONALD MUHAVI, !';
 
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
